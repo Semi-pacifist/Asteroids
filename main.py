7c32b796faa8d6b1f2f482cd19da2d09b88bfd56
Justin Robertson <semi_pacifist@yahoo.com>
2025-07-13 18:22:15 -0500

    Begin Asteroids project

---
+++ main.py
@@ -0,0 +1,13 @@
+# imports
+import pygame
+from constants import *
+
+# define functions
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
